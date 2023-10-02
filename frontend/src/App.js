28c70a3c81eca362ca7ed54005cbc52f4d4a8dd7
NaniBer <gdgr2010@gmail.com>
2023-10-02 11:03:45 +0300

    Redirects added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,8 @@
 import "./App.css";
-import { HashRouter, Route, Routes, NavLink } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import FullPage from "./Pages/FullPage";
 import DetailsPage from "./Pages/DetailsPage";
-import Intro from "./Sections/Intro";
-import Expertise from "./Sections/Expertise";
-import Footer from "./Sections/Footer";
+
 import Portfolio from "./Sections/Portfolio";
 import OurTeam from "./Sections/OurTeam";
 
