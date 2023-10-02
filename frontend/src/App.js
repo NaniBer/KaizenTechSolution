dc8235b021f7262e917d30df2c70a5173a603dc3
NaniBer <gdgr2010@gmail.com>
2023-10-02 11:41:32 +0300

    Minor changes done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,6 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<FullPage />} />
-
       <Route path="/details" element={<DetailsPage />} />
       <Route path="/portfolio" element={<Portfolio />} />
       <Route path="/ourteam" element={<OurTeam />} />
